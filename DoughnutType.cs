ea806b8c4b324c8c40752f076ba723c7181704e4
GYONGYI ELISABETA KOVACS <kovacs.gyongyi165078@student.ubbcluj.ro>
2020-10-26 13:02:10 +0200

    Laborator3

---
+++ DoughnutType.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kovacs_Gyongyi_Elisabeta_Lab2
+{
+
+
+    class Doughnut
+    {
+        private DoughnutType mFlavor;
+
+        public DoughnutType Flavor
+        {
+            get
+            {
+                return mFlavor;
+            }
+            set
+            {
+                mFlavor = value;
+            }
+        }
+        private float mPrice = .50F;
+        public float Price
+        {
+            get
+            {
+                return mPrice;
+            }
+            set
+            {
+                mPrice = value;
+            }
+        }
+        private readonly DateTime mTimeOfCreation;
+        public DateTime TimeOfCreation
+        {
+            get
+            {
+                return mTimeOfCreation;
+            }
+
+        }
+        public Doughnut(DoughnutType aFlavor) // constructor
+        {
+            mTimeOfCreation = DateTime.Now;
+            mFlavor = aFlavor;
+        }
+    }
+}
